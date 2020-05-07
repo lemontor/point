0652d755025b8bc5a25b136486178b0b26f3497a
lementor <lingangtor>
2020-05-07 21:07:05 +0800

    提交第二个页面

---
+++ point/app/src/main/java/com/yo/lg/pointutils/SecondActivity.kt
@@ -0,0 +1,12 @@
+package com.yo.lg.pointutils
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+class SecondActivity :AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+}
